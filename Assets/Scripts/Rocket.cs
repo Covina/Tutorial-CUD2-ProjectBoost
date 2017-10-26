2ab4660e57085c29f0e10a3fb66336668fbec413
Covina <jabs303@gmail.com>
2017-10-26 00:31:38 -0700

    Section 3, Lecture 37:  Initial Rocket Script

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //
+        ProcessInput();
+
+
+	}
+
+
+    // Look for keyboard input
+    private void ProcessInput()
+    {
+
+        // check for thrust
+        if(Input.GetKey(KeyCode.Space))
+        {
+
+            print("Thrusting");
+
+        }
+
+
+        // check for left tilt
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // Rotate toward left
+            print("Left Arrow Pressed");
+
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            // Rotate toward right
+            print("Right Arrow Pressed");
+
+        }
+
+
+
+    }
+
+}
