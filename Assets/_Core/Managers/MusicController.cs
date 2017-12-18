559375ba4f85d57e520288ad6a21b94855d1e92e
Covina <jabs303@gmail.com>
2017-12-18 08:53:25 -0800

    Folder restructure; Added music loop

---
+++ Assets/_Core/Managers/MusicController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+
+    public static MusicController instance;
+
+
+    void Awake()
+    {
+        MakeSingleton();    
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void MakeSingleton()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
